d81da741afbff389d0d0cc362f886a59a4bb8471
Omnivers <Twersimor.stock@outlook.com>
2022-06-23 19:39:29 +0200

    New project in work space

---
+++ Languages/FR/script/script2.js
@@ -0,0 +1,23 @@
+const txtAnim=document.getElementById('sm-heading');
+var txt = 'Un petit résumé à propos de moi...';
+var i=0;
+var speed = 100;
+
+function typeWriter() {
+    if (i < txt.length-4) {
+        txtAnim.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100);
+      }
+    else if (i<txt.length) {
+        txtAnim.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, 700);
+    }
+    else{
+        i=0;
+        txtAnim.innerHTML="";
+        typeWriter();
+        }
+}
+window.onload=typeWriter();
