8fe45bb68746c43d85551f6973085a36e130a84f
Omnivers <Twersimor.stock@outlook.com>
2022-07-11 12:10:10 +0200

    Update

--- script/main.js
+++ script/main.js
@@ -4,7 +4,6 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
